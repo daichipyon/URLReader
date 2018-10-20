fb74b225a84e20008b22305267f9f94292e250ff
vagrant <vagrant@vagrant.vm>
2018-10-20 17:38:07 +0000

    change algorithm ms to google

--- vision.py
+++ vision.py
@@ -77,10 +77,10 @@ def get_url_from_text_gg(image_url=None, image=None):
         return return_text
         
     elif image:
-        img = base64.b64encode(image).decode()
+        #img = base64.b64encode(image).decode()
         api_url = GOOGLE_CLOUD_VISION_API_URL 
         req_body = {
-                'image': {'content':img},
+                'image': {'content':imgage},
                 'features': [{
                     'type': 'DOCUMENT_TEXT_DETECTION'
                 }],
